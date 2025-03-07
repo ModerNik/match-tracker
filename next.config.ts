77389e7b7f8ff4e2191d7bb8054de1f9755c91bd
ModerNik <modernik.work@gmail.com>
2025-03-07 17:44:55 +0300

    feat: api
    added api endpoint using axios

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false,
+  poweredByHeader: false,
+  devIndicators: {
+    appIsrStatus: false,
+  },
 };
 
 export default nextConfig;
---
+++ src/api/types/base.ts
@@ -0,0 +1,4 @@
+export type BaseResponse<T> = {
+  ok: boolean;
+  data: T;
+};
