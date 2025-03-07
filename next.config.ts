507564461e553d48d645e7daed72c865f0054baa
ModerNik <modernik.work@gmail.com>
2025-03-07 19:40:44 +0300

    update: next config

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   poweredByHeader: false,
-  devIndicators: {
-    appIsrStatus: false,
-  },
 };
 
 export default nextConfig;
